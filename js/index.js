f8565c502a6ef7b98f53438e1067ee6fcf18119b
Juan A. Barrera R <jbarrera0314@gmail.com>
2021-03-04 19:03:01 -0500

    Cambio a VSCode

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
-function prueba(){
+/*function prueba(){
   alert('Branch test');
-}
+}*/
 //window.onload = prueba;
+function sum (number1,number2){
+  var number1 = parseFloat(document.getElementById("first_number").value);
+  var number2 = parseFloat(document.getElementById("second_number").value);
+  var result = number1 + number2
+  return result
+}
