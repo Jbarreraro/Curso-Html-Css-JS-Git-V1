40c276adf2eec550e2be80d95c482ed859c1ffcd
Juan A. Barrera R <jbarrera0314@gmail.com>
2021-03-06 11:38:02 -0500

    Document Object Model

--- js/index.js
+++ js/index.js
@@ -2,3 +2,6 @@ function prueba(){
   alert('Branch test');
 }
 //window.onload = prueba;
+var P_elements = document.getElementsByTagName("p");
+
+var second_paragraph = document.getElementById("second_paragraph");
